f1672c86d991fd8b47a7961de691697598dbd674
zf851 <samin_farrokhi@yahoo.com>
2020-09-09 17:21:59 +0430

    change search.php

--- search.php
+++ search.php
@@ -1,44 +1,73 @@
 <?php
-if(isset($_POST['btn'])){
-    $str=$_POST['str'];
-    //echo $str;
-    $a=mysqli_connect('localhost','root','','php_test');
-    //عینا
-    //$sql="SELECT * FROM user_tbl WHERE name LIKE 'str'";
-    //باهرچیزی شروع بشه اما بااون که کاربر می نویسه تموم بشه
-   // $sql="SELECT * FROM user_tbl WHERE name LIKE '%str'";
-    //شاملش باشه
-    $sql="SELECT * FROM user_tbl WHERE name LIKE '%str'";
-    $row=mysqli_query($a,$sql);
-    var_dump($row);
-}
-
-
-
+    if(isset($_POST['btn'])){
+        $data=$_POST['frm'];
+        $txt=$data['str'];
+        //var_dump($data);
+        //echo $str;
+        $a=mysqli_connect("localhost","root","","php_test");
+        $sql="SELECT * FROM users_tbl WHERE $data[field] LIKE '%$txt%'";
+        //echo $sql;die;
+        $row=mysqli_query($a,$sql);
+    }
 ?>
-
 <!doctype html>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<form  method="post">
+    <form method="post">
+        <input type="text" name="frm[str]" />
+            <select name="frm[field]">
+                <option value="name">name</option>
+                <option value="lastname">lastname</option>
+                <option value="age">age</option>
+                <option value="field">field</option>
+            </select>
+        <input type="submit" name="btn" />
+    </form>
+    <?php  if(isset($_POST['btn'])){ ?>
+    <table class="maintbl">
+        <tr>
+            <th>Name</th>
+            <th>Lastname</th>
+            <th>Age</th>
+            <th>Field</th>
+            <th>Comment</th>
+            <th>Picture</th>
+            <th>Delete</th>
+            <th>edit</th>
+        </tr>
 
-    <input type="text" name="str">
-    <input type="submit" name="btn">
-</form>
+        <?php
+        while($res=mysqli_fetch_array($row)){
+            ?>
+            <tr>
+                <td><?php echo $res['name']; ?></td>
+                <td><?php echo $res['lastname']; ?></td>
+                <td><?php echo $res['age']; ?></td>
+                <td><?php echo $res['field']; ?></td>
+                <td><?php echo $res['comment']; ?></td>
+                <td><img src="<?php echo $res['picture']; ?>" height="30" /></td>
+                <td>
+                    <a href="delete.php?id=<?php echo $res['user_id']; ?>">
+                        <img src="delete.png" width="20" />
+                    </a>
+                </td>
+                <td>
+                    <a href="edit.php?id=<?php echo $res['user_id']; ?>">
+                        <img src="edit.png" width="20" />
+                    </a>
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
 
-<?php
-while($res=mysqli_fetch_array($row)){
-    echo "<li>".$res['name']."</li>";
-}
-?>
-</ul>
+
+    </table>
+    <?php } ?>
 </body>
 </html>
